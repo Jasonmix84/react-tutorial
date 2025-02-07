21865799bcb01ff5bb86b97e6d04411cf0f448b4
Jason Rojas <jasonmix84@gmail.com>
2025-02-06 23:12:36 -0500

    Creating UI using Tailwind and use map to loop through state array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {name: "Caleb", role: "Backend Dev", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Jason", role: "Boyfriend", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Abi", role: "Girlfriend", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Rick", role: "Frontend Dev", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Jay", role: "Psychologist", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "David", role: "Chemist", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Sam", role: "Physiologist", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Ryan", role: "Database Manager", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Reanne", role: "Composer", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+      {name: "Liz", role: "Psychiritrist", img: "https://images.pexels.com/photos/1751542/pexels-photo-1751542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+    ]
+  );
   const showEmployees = true;
   return (
     <div className="App">
@@ -14,11 +29,19 @@ function App() {
           setRole(e.target.value);
         }}
         />
-          <Employee name=' Caleb' role='intern'/>
-          <Employee name=' Jason' role = 'boyfriend'/>
-          <Employee name = ' Abi' role = 'girlfriend'/>
-          <Employee name = ' Jay' role = {role}/>
-
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {
+            return (
+              <Employee
+                key={uuidv4()}
+                name={employee.name}
+                role = {employee.role}
+                img = {employee.img}
+                />
+            );
+          })}
+        </div>
+          
         </> 
       ) : (
         <p>You cannot see the employees</p>
